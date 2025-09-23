f3e8423c87bf37fcc36d55b3566738a6a998bc61
sergiuxmas <sergiu.xmas@gmail.com>
2025-09-23 17:40:53 +0300

    email is uniq field in Participant class

--- src/main/java/com/endava/internship/meeting/Participant.java
+++ src/main/java/com/endava/internship/meeting/Participant.java
@@ -20,8 +20,22 @@ public final class Participant {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Participant)) return false;
+        Participant other = (Participant) o;
+        // Equality based solely on email
+        return email.equals(other.email);
+    }
+
+    @Override
+    public int hashCode() {
+        // Must align with equals: only email contributes
+        return email.hashCode();
+    }
+
+    @Override
     public String toString() {
         return name + " <" + email + ">";
     }
 }
-
